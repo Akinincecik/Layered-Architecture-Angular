50da7c565246c21c10af42792505189c37c32d0f
akın incecik <akinincecik@gmail.com>
2025-01-17 20:32:58 +0300

    Can request from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
+import { NaviComponent } from "./components/navi/navi.component";
+import { CategoryComponent } from "./components/category/category.component";
+import { ProductComponent } from "./components/product/product.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, NaviComponent, CategoryComponent, ProductComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'aegis-combat-system';
+  title:string = 'aegis-combat-system';
 }
